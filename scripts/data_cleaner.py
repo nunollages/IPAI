c23e0c6c626bad4970d90578d7d10dc57b467657
Nuno Lages <nuno.lages.lourenco@gmail.com>
2026-03-19 16:10:22 +0000

    Limpeza dos dados

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -11,6 +11,18 @@ reviews_df = pd.read_csv(REVIEWS, sep=';')
 # Songs dataset cleaning
 songs_df.info()
 songs_df = songs_df.dropna(subset=['album_name'])
+songs_df = songs_df[~(songs_df['lyrics'].str.contains(r'\[Instrumental\]', na=False))]
 
+# Artists dataset cleaning 
+artists_df.info()
+artists_df = artists_df.dropna()
 
+# Reviews dataset cleaning 
+reviews_df.info()
+reviews_df = reviews_df.dropna()
+reviews_df = reviews_df.drop_duplicates()
 
+# Export the cleaned data to a csv file
+songs_df.to_csv("data/clean_songs.csv", index=False)
+artists_df.to_csv("data/clean_artists.csv", index=False)
+reviews_df.to_csv("data/clean_reviews.csv", index=False)
